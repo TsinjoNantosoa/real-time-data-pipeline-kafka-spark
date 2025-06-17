935bfe809ebeab6f0f415a1ce93be869912e058c
TsinjoNantosoa <tsinjonantosoa@gmail.com>
2025-06-17 23:31:54 +0300

    amelioration

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 set -e
+
+# Installer les dépendances si présentes
 if [ -e "/opt/airflow/requirement.txt" ]; then
-  $(command -v pip) install --user -r requirement.txt
+  pip install -r /opt/airflow/requirement.txt
 fi
 
+# Initialisation de la DB et création de l'utilisateur admin si la base n'existe pas
 if [ ! -f "/opt/airflow/airflow.db" ]; then
-  airflow db init && \
+  airflow db init
   airflow users create \
-  -- username admin \
-  -- firstname admin \
-  -- lastname admin \
-  -- role Admin \
-  -- email admin@example.com \
-  -- password admin
+    --username admin \
+    --firstname admin \
+    --lastname admin \
+    --role Admin \
+    --email admin@example.com \
+    --password admin
 fi
 
-$(command -v airflow) db upgrade
+# Upgrade toujours la DB au cas où
+airflow db upgrade
 
+# Démarre le webserver
 exec airflow webserver
