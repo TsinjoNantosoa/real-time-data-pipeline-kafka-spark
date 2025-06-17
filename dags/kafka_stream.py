f6ef69d28ee179a21252ac99999d8f5e271afe45
TsinjoNantosoa <tsinjonantosoa@gmail.com>
2025-06-17 21:58:00 +0300

    add an airflow

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,92 +1,56 @@
 import json
-from datetime import datetime
-from airflow import DAG
-from airflow.providers.standard.operators.python import PythonOperator
+import requests
+from kafka import KafkaProducer
 
-# from airflow.operators.python import PythonOperator  # corrected operator path
-
-default_args = {
-    'owner': 'tsinjo',
-    'start_date': datetime(2023, 8, 3, 10, 0)
-}
 
 def get_data():
-    import requests
     res = requests.get("https://randomuser.me/api/")
     res = res.json()
-    res = res['results'][0]
-    return res
+    return res['results'][0]
 
 
 def format_data(res):
-    data = {}
-
-    # Informations générales
-    data['gender'] = res['gender']
-    data['title'] = res['name']['title']
-    data['first_name'] = res['name']['first']
-    data['last_name'] = res['name']['last']
-
-    # Localisation
-    location = res['location']
-    data['address'] = f"{location['street']['number']} {location['street']['name']}"
-    data['city'] = location['city']
-    data['state'] = location['state']
-    data['country'] = location['country']
-    data['postcode'] = location['postcode']
-    data['latitude'] = location['coordinates']['latitude']
-    data['longitude'] = location['coordinates']['longitude']
-    data['timezone_offset'] = location['timezone']['offset']
-    data['timezone_description'] = location['timezone']['description']
-
-    # Contact
-    data['email'] = res['email']
-    data['phone'] = res['phone']
-    data['cell'] = res['cell']
-
-    # Identifiants
-    data['uuid'] = res['login']['uuid']
-    data['username'] = res['login']['username']
-
-    # Date de naissance et inscription
-    data['birthdate'] = res['dob']['date']
-    data['age'] = res['dob']['age']
-    data['registered_date'] = res['registered']['date']
-    data['registered_age'] = res['registered']['age']
-
-    # Nationalité et ID
-    data['nationality'] = res['nat']
-    data['id_name'] = res['id']['name']
-    data['id_value'] = res['id']['value']
-
-    # Images
-    data['picture_thumbnail'] = res['picture']['thumbnail']
-    data['picture_medium'] = res['picture']['medium']
-    data['picture_large'] = res['picture']['large']
-
+    data = {
+        'gender': res['gender'],
+        'title': res['name']['title'],
+        'first_name': res['name']['first'],
+        'last_name': res['name']['last'],
+        'address': f"{res['location']['street']['number']} {res['location']['street']['name']}",
+        'city': res['location']['city'],
+        'state': res['location']['state'],
+        'country': res['location']['country'],
+        'postcode': res['location']['postcode'],
+        'latitude': res['location']['coordinates']['latitude'],
+        'longitude': res['location']['coordinates']['longitude'],
+        'timezone_offset': res['location']['timezone']['offset'],
+        'timezone_description': res['location']['timezone']['description'],
+        'email': res['email'],
+        'phone': res['phone'],
+        'cell': res['cell'],
+        'uuid': res['login']['uuid'],
+        'username': res['login']['username'],
+        'birthdate': res['dob']['date'],
+        'age': res['dob']['age'],
+        'registered_date': res['registered']['date'],
+        'registered_age': res['registered']['age'],
+        'nationality': res['nat'],
+        'id_name': res['id']['name'],
+        'id_value': res['id']['value'],
+        'picture_thumbnail': res['picture']['thumbnail'],
+        'picture_medium': res['picture']['medium'],
+        'picture_large': res['picture']['large']
+    }
     return data
 
 
 def stream_data():
-    import json
-    from kafka import KafkaProducer
-    import time
     res = get_data()
     res = format_data(res)
-    # print(json.dumps(res, indent=3))
-    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
-    producer.send('users_created' , json.dumps(res).encode('utf-8'))
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    producer.send('users_created', json.dumps(res).encode('utf-8'))
+    producer.flush()
+    print("✅ Message envoyé dans 'users_created'")
 
 
-# with DAG(
-#     'user_automation',
-#     default_args=default_args,
-#     schedule='@daily',
-#     catchup=False
-# ) as dag:
-#     streaming_task = PythonOperator(
-#         task_id='stream_data_from_api',
-#         python_callable=stream_data
-#     )
-
-stream_data()
+if __name__ == "__main__":
+    stream_data()
